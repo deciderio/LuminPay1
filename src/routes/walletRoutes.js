15f9c855fc461321eabdb994b4666d60248de464
Tec. Deciderio <149083358+deciderio@users.noreply.github.com>
2025-09-18 04:01:21 -0600

    Delete src directory
    Signed-off-by: Tec. Deciderio <149083358+deciderio@users.noreply.github.com>

--- src/routes/walletRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const walletController = require('../controllers/walletController');
-
-// Ruta para obtener el saldo de la billetera de un usuario
-// GET /api/wallets/:userId/balance
-router.get('/:userId/balance', walletController.getWalletBalance);
-
-module.exports = router;
